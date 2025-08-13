def3a46c6b5f8f7cd2f27a780c4a356916b16a0e
Felix Michael Thottumkal <felix-macbookair@MacBook-Air-Felix.local>
2025-08-13 08:39:23 +0100

    Fix: Read Sanity vars from NEXT_PUBLIC_ or server vars

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,10 +1,19 @@
 import { createClient } from '@sanity/client'
 
+const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_PROJECT_ID
+const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_DATASET
+
+if (!projectId || !dataset) {
+  throw new Error('Missing Sanity env: projectId/dataset')
+}
+
 export const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID!,
-  dataset: process.env.SANITY_DATASET!,
+  projectId,
+  dataset,
   apiVersion: '2025-07-01',
   useCdn: true,
   token: process.env.SANITY_API_READ_TOKEN || undefined,
-  perspective: 'published'
+  perspective: 'published',
 })
